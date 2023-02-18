013566bdb969cfa353ee1c6e04a49011ce60d837
abhijeetkumarsinghofficial <hackker4200@gmail.com>
2023-02-18 11:30:25 +0530

    added navbar

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 // import App from './App';
 // import Cart from './Cart';
 import CartList from './CartList';
+import Navbar from './Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
+    <Navbar/>
     <CartList />
 
 
